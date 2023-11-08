40dcf473e294f352109000f7bdb1332f7ddca589
dochoul <dochoul@gmail.com>
2023-11-08 15:43:44 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [books, setBooks] = useState([]);
+
+  async function fetchData() {
+    const response = await axios.get(
+      "https://85c3ac80-6126-4378-bfb0-43dbff5aa142.mock.pstmn.io/list"
+    );
+    setData(response);
+    //console.log(response);
+  }
+
+  async function createData() {
+    const response = await axios.post(
+      "https://85c3ac80-6126-4378-bfb0-43dbff5aa142.mock.pstmn.io/list",
+      { name: "아이언맨", age: 100, male: "M" }
+    );
+    console.log(response);
+  }
+
+  async function fetchBooks() {
+    const response = await axios.get(
+      "/v1/search/book.json?query=주식&display=5&start=1",
+      {
+        headers: {
+          "X-Naver-Client-Id": "ky8lLbnB0L4E8EH2gm73",
+          "X-Naver-Client-Secret": "F6ZVyV2Qiz",
+        },
+      }
+    );
+    setBooks(response.data.items);
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+    // fetchData();
+    // createData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {books.map((book) => (
+          <div key={book.isbn}>
+            <h2>{book.title}</h2>
+            <a
+              href={book.link}
+              target="_blank"
+              title="새창"
+              rel="noreferer noreferrer"
+            >
+              <img src={book.image} alt="" />
+              <p>{book.author}</p>
+              <p>{book.publisher}</p>
+              <p>{book.description}</p>
+              <p>{book.pubdate}</p>
+              <p>{book.discount}</p>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
