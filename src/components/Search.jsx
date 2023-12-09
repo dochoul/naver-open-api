83357f75c75e98d0c7e43021c9b1338f4e7d75ed
dochoul@gmail.com <dochoul@gmail.com>
2023-12-09 17:34:29 +0900

    검색창 코드 수정

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,70 @@
 import { useState } from "react";
-import { INITIAL_DISPLAY_BOOKS } from "../constant";
+import {
+  BROWSER_LARGE_SIZE,
+  BROWSER_MEDIUM_SIZE,
+  INITIAL_DISPLAY_BOOKS,
+} from "../constant";
 import { fetchBooks } from "../apis";
+import styled from "styled-components";
+
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto 2%;
+  width: 95%;
+  @media only screen and (min-width: ${BROWSER_MEDIUM_SIZE}) {
+  }
+  @media only screen and (min-width: ${BROWSER_LARGE_SIZE}) {
+    width: 50%;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px 15px;
+  border: 10px solid #00c772;
+  border-width: 5px;
+  border-radius: 6px;
+  height: 30px;
+  font-size: 16px;
+  transition: border-color 0.15s ease-in;
+  @media only screen and (min-width: ${BROWSER_MEDIUM_SIZE}) {
+    border-width: 8px;
+    font-size: 18px;
+  }
+  @media only screen and (min-width: ${BROWSER_LARGE_SIZE}) {
+    font-size: 20px;
+  }
+`;
+
+const Button = styled.button`
+  position: absolute;
+  right: 12px;
+  top: calc(50% - 19px);
+  cursor: pointer;
+  @media only screen and (min-width: ${BROWSER_MEDIUM_SIZE}) {
+    top: calc(50% - 24px);
+  }
+  @media only screen and (min-width: ${BROWSER_LARGE_SIZE}) {
+    top: calc(50% - 24px);
+  }
+`;
+
+const SearchImage = styled.img`
+  width: 36px;
+  height: 36px;
+  @media only screen and (min-width: ${BROWSER_MEDIUM_SIZE}) {
+    width: 46px;
+    height: 46px;
+  }
+  @media only screen and (min-width: ${BROWSER_LARGE_SIZE}) {
+    width: 46px;
+    height: 46px;
+  }
+`;
 
 const Search = ({ searchBooks }) => {
   const [keyword, setKeyword] = useState("");
@@ -25,19 +89,19 @@ const Search = ({ searchBooks }) => {
   };
 
   return (
-    <form onSubmit={$_handleSubmit} className="search_wrap">
-      <input
+    <Form onSubmit={$_handleSubmit}>
+      <Input
         value={keyword}
         onChange={$_handleKeyword}
         placeholder="한 시간 정도 독서하면 어떤 고통도 진정된다."
       />
-      <button type="submit" className="search_button">
-        <img
+      <Button type="submit">
+        <SearchImage
           src="https://www.semie.cooking/assets/images/common/ic_search.png"
           alt=""
         />
-      </button>
-    </form>
+      </Button>
+    </Form>
   );
 };
 
